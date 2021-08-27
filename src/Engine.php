e3d1d53d88c83fd7ac45dc500ab5b4cbf6b4bbb5
Денис Столяров <skwidw92@gmail.com>
2021-08-27 17:11:43 +0300

    fix: lint

--- src/Engine.php
+++ src/Engine.php
@@ -12,6 +12,7 @@ use function cli\prompt;
 
 function engine(string $nameGame)
 {
+    $data = [];
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
